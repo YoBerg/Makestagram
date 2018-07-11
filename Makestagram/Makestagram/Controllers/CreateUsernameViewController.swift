49291a754b038e0ff3da518c9f29ce99ff3160ea
Yohan Berg <yohan520@gmail.com>
2018-07-11 16:59:31 -0700

    Added Login storyboard
    Added an incomplete draft of the login storyboard.

---
+++ Makestagram/Makestagram/Controllers/CreateUsernameViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreateUsernameViewController.swift
+//  Makestagram
+//
+//  Created by Macbook on 7/11/18.
+//  Copyright © 2018 Yohan Berg. All rights reserved.
+//
+
+import UIKit
+
+class CreateUsernameViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        
+    }
+}
